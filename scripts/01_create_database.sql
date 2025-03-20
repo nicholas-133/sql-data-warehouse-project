5598c8bcd9a8127da62bd01c0e58b6d92659b98f
nicholas-133 <133066368+nicholas-133@users.noreply.github.com>
2025-03-20 08:47:35 -0700

    Add files via upload

---
+++ scripts/01_create_database.sql
@@ -0,0 +1,35 @@
+/*
+===============================================
+Create Database Schemas
+===============================================
+Script Purpose:
+	This scripts creates a new database named 'DataWarehouse' after checking if it already exists.
+	If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas within the database: 'Bronze', 'Silver', and 'Gold'. 
+
+VARNING: 
+	Running this script will drop the entire 'DataWarehouse' database if it exists.
+	All data in the database will be permanently deleted. 
+	Proceed with caution and ensure you have proper backups before running this script.
+*/
+
+USE master;
+GO
+
+-- Create Database DataWarehouse
+CREATE Database DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
+
+--
